d176531fe06b53ad0b19afe2b2e448240b805c56
David <volkskii@gmail.com>
2017-07-26 15:49:17 -0400

    Add files via upload

---
+++ FahrToCelsius.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FahrToCelsius {
+	public static void main(String[] args) {
+
+	Scanner input = new Scanner(System.in);
+	
+	System.out.print("Enter degrees in Fahrenheit: ");
+	double fahr = input.nextDouble();
+	
+	double celsius = (5.0/9) * (fahr - 32);
+	System.out.println(fahr + " degrees in Fahrenheit = " + celsius + " degrees in Celsius.");
+	}
+}
